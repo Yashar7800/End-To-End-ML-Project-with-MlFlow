6abaf566a8ba764de54e9f4bfc4b2a59fee707d2
Yashar <yashar1378.ghaffari@gmail.com>
2025-06-19 15:18:17 +0200

    Model Evaluation and MLflow added

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -33,3 +33,14 @@ class ModelTrainerConfig:
     alpha: float
     l1_ratio: float   # will define L1 ratio in params.yaml
     target_column: str  # will get target column from schema.yaml
+
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    test_data_path: Path
+    model_path: Path
+    all_params: dict
+    metric_file_name: Path
+    target_column: str
+    mlflow_uri: str
